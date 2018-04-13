4aa24becde89af3c5ac4d9a053ff13df78023883
Olivier Bellone <ob@stripe.com>
2018-04-13 15:38:29 +0200

    Bump stripe-mock to 0.12.0

--- src/test/java/com/stripe/BaseStripeMockTest.java
+++ src/test/java/com/stripe/BaseStripeMockTest.java
@@ -32,7 +32,7 @@ import org.mockito.ArgumentMatcher;
 import org.mockito.Mockito;
 
 public class BaseStripeMockTest {
-  private static final String MOCK_MINIMUM_VERSION = "0.11.2";
+  private static final String MOCK_MINIMUM_VERSION = "0.12.0";
 
   private static String port;
 
